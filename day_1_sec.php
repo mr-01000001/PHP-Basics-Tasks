c3a3b7a5d69d27f09df759cccc8681c90a16ab89
Ahmed Muhammed <75481578+mr-01000001@users.noreply.github.com>
2021-09-02 19:52:14 +0200

    Add files via upload

---
+++ day_1_sec.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>	
+
+<head>
+	<title> Second </title>
+</head>
+
+<body style="background-color: #F7FF49;">
+
+	<?php
+		$a = "Hello PHP. ";		// intialize Var & insert value.
+		echo "<u> <h1> $a <h1> </u> <br>";		// print variable.
+	?>
+	
+</body>
+
+</html>
