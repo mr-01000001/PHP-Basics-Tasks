7d437a9c4ded710f531609a4be2222d6b81a4cd0
Ahmed Muhammed <75481578+mr-01000001@users.noreply.github.com>
2021-09-02 19:53:30 +0200

    Add files via upload

---
+++ 3.php
@@ -0,0 +1,9 @@
+<?php
+	$text = "hello world! <br>";
+	$text1 = "HELLO PHP. <br>";
+
+	echo strtoupper($text);			// string all uppercase letters.
+	echo strtolower($text1); 		// string all lowercase letters.
+	echo ucfirst($text);			// string's first character uppercase.
+	echo ucwords($text); 			// string's first character of all the words uppercase.
+?>
