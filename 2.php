640494565a6ec93e154ed661f23c9a015ddacbcf
Ahmed Muhammed <75481578+mr-01000001@users.noreply.github.com>
2021-09-02 19:53:17 +0200

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,12 @@
+<?php
+# define variable & assign a value. 
+$d = "A00";
+$a = "<br>";
+
+# print with increment variable.
+echo ++$d, $a;		 // A01
+echo ++$d, $a;		 // A02
+echo ++$d, $a;		 // A03
+echo ++$d, $a;		 // A04
+echo ++$d; 		     // A05
+?>
