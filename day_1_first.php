c4e209001f592553ade5799926e2e1a8df967771
Ahmed Muhammed <75481578+mr-01000001@users.noreply.github.com>
2021-09-02 19:51:47 +0200

    Add files via upload

---
+++ day_1_first.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title> First </title>
+</head>
+
+<body style = "background-color: #DF8DFC;">
+
+	<?php
+		echo "<center> <h1> Hello World. </h1> </center> <br>";
+	?>
+	
+</body>
+
+</html>
