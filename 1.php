183eccf92e4a7bf62bdc82ee5a7e9adf063a9b26
Ahmed Muhammed <75481578+mr-01000001@users.noreply.github.com>
2021-09-02 19:53:03 +0200

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> 1 </title>
+	</head>
+	<body>
+		<?php
+			$var = "PHP Tutorial";		// initial variable.
+			echo "<h3> $var <h3>";		// print var in "h3" tag.
+		?>
+	</body>
+</html>
